809a42760b084504d6ee797df101e18374ca777a
MartaJ <marthajensen@utah.gov>
2022-12-14 16:07:06 -0700

    delete

--- python_scripts/groundwater_summary_new.bat
+++
@@ -1,1 +0,0 @@
-"C:/Users/marthajensen/AppData/Local/ESRI/conda/envs/arcgispro-py3-clone/python.exe" "C:/ArcpyScripts/ugsgwp_reading_flow_summary_tables.py" pause
--- python_scripts/post_reconcile_ugsgwp.bat
+++
@@ -1,1 +0,0 @@
-"C:\Users\marthajensen\AppData\Local\ESRI\conda\envs\PRO3.0clone\python.exe" "C:\ArcpyScripts\reconcile_post_ugsgwp.py" cmd /k
