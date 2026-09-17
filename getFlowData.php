9d6a98fd551cd4bcdce665e3d778fd4c77e24dcd
marshallrobinson <marshallrobinson@users.noreply.github.com>
2026-09-17 15:40:19 -0600

    fix: parameterize locID flow query with pg_query_params (SQLi)

--- getFlowData.php
+++ getFlowData.php
@@ -43,20 +43,23 @@
 
 	
 	$temp = explode(",", $locID);
+	// Bind the validated ids as $1,$2,... placeholders (pg_query_params) instead of interpolating.
+	$ph = array();
 	for($i = 0; $i < count($temp); $i++)
 	{
-		$locIDs .= "'" . $temp[$i] . "',";
+		$ph[] = '$' . ($i + 1);
 	}
-	$locIDs = rtrim($locIDs, ',');
+	$flowParams = $temp;
 
 	if($type == "daily")
 	{
 		// OLD $sql = "SELECT CONVERT(VARCHAR(10), FLOWDATE, 101) as FlowDate, CAST(FLOWDATE as Date) AS dtOrder, Sum(Discharge) / COUNT(CAST(FLOWDATE as Date)) as Discharge, LOCATIONID as LocationID from UGGPADMIN.UGS_GW_FLOW where LOCATIONID IN (" . $locIDs . ") group by CONVERT(VARCHAR(10), FLOWDATE, 101), CAST(FLOWDATE as Date), LOCATIONID ORDER BY CAST(FLOWDATE as date);";
 		//$sql = "SELECT to_char(FLOWDATE, 'MM/DD/YYYY') as FlowDate, CAST(FLOWDATE as Date) AS dtOrder, TRUNC(Sum(Discharge) / COUNT(CAST(FLOWDATE as Date)),6) as Discharge, LOCATIONID as LocationID from UGS_GW_FLOW where LOCATIONID IN (" . $locIDs . ") group by to_char(FLOWDATE, 'MM/DD/YYYY'), CAST(FLOWDATE as Date), LOCATIONID ORDER BY CAST(FLOWDATE as date);";
-		$sql = "SELECT to_char(FLOWDATE, 'MM/DD/YYYY') as FlowDate, TRUNC(Sum(Discharge) / COUNT(CAST(FLOWDATE as Date)),4) as Discharge from UGS_GW_FLOW where LOCATIONID IN (" . $locIDs . ") group by to_char(FLOWDATE, 'MM/DD/YYYY'), CAST(FLOWDATE as Date) ORDER BY CAST(FLOWDATE as date);";
+		$sql = "SELECT to_char(FLOWDATE, 'MM/DD/YYYY') as FlowDate, TRUNC(Sum(Discharge) / COUNT(CAST(FLOWDATE as Date)),4) as Discharge from UGS_GW_FLOW where LOCATIONID IN (" . implode(',', $ph) . ") group by to_char(FLOWDATE, 'MM/DD/YYYY'), CAST(FLOWDATE as Date) ORDER BY CAST(FLOWDATE as date);";
 	}
 	else if($type == "monthly")
 	{
+		$flowParams = array();
 		//old $sql = "SELECT CAST(MONTH(FLOWDATE) AS varchar) + '/' + CAST(YEAR(FLOWDATE) AS varchar) as FlowDate, CAST(YEAR(FLOWDATE) AS varchar) + '-' + RIGHT ('00'+ CAST (MONTH(FLOWDATE) AS varchar), 2) as dtOrder, Sum(Discharge) / COUNT(CAST(YEAR(FLOWDATE) AS varchar) + '-' + RIGHT ('00'+ CAST (MONTH(FLOWDATE) AS varchar), 2)) as Discharge, LOCATIONID as LocationID from UGGPADMIN.UGS_GW_FLOW where LOCATIONID IN (" . $locIDs . ") group by CAST(YEAR(FLOWDATE) AS varchar) + '-' + RIGHT ('00'+ CAST (MONTH(FLOWDATE) AS varchar), 2), CAST(MONTH(FLOWDATE) AS varchar) + '/' + CAST(YEAR(FLOWDATE) AS varchar), LOCATIONID order by dtOrder;";
 		//$sql = "SELECT CAST(EXTRACT(MONTH FROM flowdate) as varchar) || '/' || CAST(EXTRACT(YEAR FROM flowdate) AS varchar) as FlowDate, CAST(EXTRACT(YEAR FROM flowdate) AS varchar) || '-' || RIGHT ('00' || CAST(EXTRACT(MONTH FROM flowdate) AS varchar), 2) as dtOrder, TRUNC(Sum(Discharge) / COUNT(CAST(EXTRACT(YEAR FROM flowdate) AS varchar) || '-' || RIGHT ('00' || CAST(EXTRACT(MONTH FROM flowdate) AS varchar), 2)),4) as Discharge, LOCATIONID as LocationID from UGS_GW_FLOW where LOCATIONID IN (10010) group by CAST(EXTRACT(YEAR FROM flowdate) AS varchar) || '-' || RIGHT('00' || CAST(EXTRACT(MONTH FROM flowdate) AS varchar), 2), CAST(EXTRACT(MONTH FROM flowdate) AS varchar) || '/' || CAST(EXTRACT(YEAR FROM flowdate) AS varchar), LOCATIONID order by dtOrder;";
 		$sql = "SELECT CAST(EXTRACT(MONTH FROM flowdate) as varchar) || '/' || CAST(EXTRACT(YEAR FROM flowdate) AS varchar) as FlowDate, CAST(EXTRACT(YEAR FROM flowdate) AS varchar) || '-' || RIGHT ('00' || CAST(EXTRACT(MONTH FROM flowdate) AS varchar), 2) as dtOrder, TRUNC(Sum(Discharge) / COUNT(CAST(EXTRACT(YEAR FROM flowdate) AS varchar) || '-' || RIGHT ('00' || CAST(EXTRACT(MONTH FROM flowdate) AS varchar), 2)),4) as Discharge from UGS_GW_FLOW where LOCATIONID IN (10010) group by CAST(EXTRACT(YEAR FROM flowdate) AS varchar) || '-' || RIGHT('00' || CAST(EXTRACT(MONTH FROM flowdate) AS varchar), 2), CAST(EXTRACT(MONTH FROM flowdate) AS varchar) || '/' || CAST(EXTRACT(YEAR FROM flowdate) AS varchar) order by dtOrder;";
@@ -68,7 +71,7 @@
 	
 
 	/// $res = mssql_query($sql);
-	$res = pg_query($conn, $sql);
+	$res = pg_query_params($conn, $sql, $flowParams);
 
 	if($res === false)
 	{
